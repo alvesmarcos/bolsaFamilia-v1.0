e499d4ea341671870c64476798b17d37e0b753bf
Marcos Alves <alves.henrique.marcos@gmail.com>
2017-03-31 22:50:58 -0300

    Integração com Barcode Scanner

--- src/pages/interatividade/interatividade.ts
+++ src/pages/interatividade/interatividade.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from 'ionic-native';
 
 @Component({
   selector: 'page-interatividade',
@@ -8,5 +9,15 @@ import { NavController } from 'ionic-angular';
 })
 
 export class InteratividadePage {
+  private _content: string;
+
   constructor(private _navCtrl: NavController){ }
+
+  public qrCodeDecode(): void {
+    BarcodeScanner.scan().then((barcodeData) => {
+       this._content = barcodeData.text;
+      }, (err) => {
+        alert("Erro");
+      });
+  }
 }
